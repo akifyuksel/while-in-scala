5b5bd0aac23f9fa282221eb05687aa452d8fba4b
akifyuksel <akif42@live.nl>
2022-04-26 17:22:56 +0200

    removed parse step and implemented the first steps
    todo: fix command and while

--- src/While.scala
+++ src/While.scala
@@ -1,20 +1,20 @@
-scala.util.parsing.combinator.JavaTokenParsers
+//scala.util.parsing.combinator.JavaTokenParsers
 
 case class NotImplementedException(s: String) extends RuntimeException(s)
-case class ParseException(s: String) extends RuntimeException(s)
+case class StepException(s: String) extends RuntimeException(s)
 case class DeriveException(s: String) extends RuntimeException(s)
 
-// somehow convert string to BExpr, still looking into that
-  def read(text: String): BExpr = {
-}
+// ---skip for now--- somehow convert string to BExpr, still looking into that
+//  def read(text: String): BExpr = {
+//}
 
-//BExpr, or Basic Expression.
+//BExpr, for Basic Syntax.
 sealed abstract class BExpr
 case class BNum(n: Int) extends BExpr
 case class BStr(s:String) extends BExpr
 case class BList(list: List[BExpr]) extends BExpr
 
-//DExpr, or Derived Expression.
+//DExpr, for Derived Syntax.
 sealed abstract class DExpr
 case class DTrue() extends DExpr
 case class DFalse() extends DExpr
@@ -23,18 +23,28 @@ case class DPlus(l: DExpr, r: DExpr) extends DExpr
 case class DLeq(l: DExpr, r: DExpr) extends DExpr
 case class DId(c: String) extends DExpr
 
-case class DCommand(l:DExpr, r:DExpr) extends DExpr //???
+case class DCommand(l:DExpr, r:DExpr) extends DExpr // should be trait
 case class DAssign(id:DId, e:DExpr) extends DExpr
 case class DPrint(e: DExpr) extends DExpr
-case class DWhile(e:DExpr, c:DCommand) extends DExpr//???
+case class DWhile(e:DExpr, c:DCommand) extends DExpr // change according to fixing command
 
 case class DDone() extends DExpr
 
-object Parser {
-  def parse(str: String): DExpr = parse(read(str))
+object Step {
+//  def step(str: String): DExpr = parse(read(str))
 
-  def parse(bexpr: BExpr): DExpr = bexpr match {
-    case _ => throw new ParseException("not yet implemented")
+  def step(bexpr: BExpr): DExpr = bexpr match {
+    case BNum(n) => DNum(n) // assuming dat parser elk getal in een BNum omzet
+    case BStr(s) => s match {
+      case "true" => DTrue()
+      case "false" => DFalse()
+      case x => DId(x) // free identifier
+    }
+    case BList(list) => list match {
+      case l :: BStr("+") :: r :: Nil => DPlus(step(l), step(r))
+      case l :: BStr("<=") :: r :: Nil => DLeq(step(l), step(r))
+      case x => throw new StepException("not yet implemented")
+    }
   }
 }
 
