e3c2a307b0621f83b75c11d413e92ea52f68b5f4
akifyuksel <akif42@live.nl>
2022-04-29 01:25:03 +0200

    worked on interpreting while (+test)

--- src/Test.scala
+++ src/Test.scala
@@ -5,27 +5,43 @@ import Step.step
 import org.scalatest.FunSuite
 
 class Test extends FunSuite {
+  // 5
   test("derive 5") {
     assertResult(NumO(5)) {
       deriveExpr(step(BNum(5)))._1
     }
   }
 
+  // (x := 27)
   test("derive assign") {
-    assertResult(DDone(), NumO(27), List(Assign("x", DNum(27)))) {
+    assertResult(DDone(), NumO(27), List(Assign("x", NumO(27)))) {
       derive(step(BList(List(BStr("x"), BStr(":="), BNum(27)))))
     }
   }
 
+  // (+ := 1)
   test("derive assign keyword identifier") {
     intercept[StepException] {
       step(BList(List(BStr("+"), BStr(":="), BNum(1))))
     }
   }
 
+  // ((x := 7) ; (print (5 + x)))
   test("derive seq") {
-    assertResult(DDone(), NumO(12), List(Assign("x", DNum(7)))) {
+    assertResult(DDone(), NumO(12), List(Assign("x", NumO(7)))) {
       derive(step(BList(List(BList(List(BStr("x"), BStr(":="), BNum(7))), BStr(";"), BList(List(BStr("print"), BList(List(BNum(5), BStr("+"), BStr("x")))))))))
+    }
+  }
+
+  // (((x := 0) ; (while (x <= 10) do (x := (x + 1)) od)) ; (print x))
+  test("derive while") {
+  assertResult(NumO(11)) {
+    derive(step(
+      BList(List(BList(List(BList(List(BStr("x"), BStr(":="), BNum(0))), BStr(";"),
+        BList(List(BStr("while"), BList(List(BStr("x"), BStr("<="), BNum(10))), BStr("do"),
+          BList(List(BStr("x"), BStr(":="), BList(List(BStr("x"), BStr("+"), BNum(1))))), BStr("od"))))), BStr(";"),
+        BList(List(BStr("print"), BStr("x")))))
+      ))._2
     }
   }
 }
--- src/While.scala
+++ src/While.scala
@@ -8,28 +8,28 @@ class DeriveException(s: String) extends RuntimeException(s)
 //  def read(text: String): BExpr = {
 //}
 
-//BExpr, for Basic Syntax.
-sealed abstract class BExpr
-case class BNum(n: Int) extends BExpr
-case class BStr(s:String) extends BExpr
-case class BList(list: List[BExpr]) extends BExpr
+//BSyn, for Basic Syntax.
+sealed abstract class BSyn
+case class BNum(n: Int) extends BSyn
+case class BStr(s:String) extends BSyn
+case class BList(list: List[BSyn]) extends BSyn
 
-//DExpr, for Derived Expressions.
-sealed abstract class DExpr
+//DSyn, for Derived syntax.
+sealed abstract class DSyn
 
 //expressions
-case class DTrue() extends DExpr //literal
-case class DFalse() extends DExpr //literal
-case class DNum(n: Int) extends DExpr //literal
-case class DPlus(l: DExpr, r: DExpr) extends DExpr
-case class DLeq(l: DExpr, r: DExpr) extends DExpr
-case class DId(c: String) extends DExpr
+case class DTrue() extends DSyn //literal
+case class DFalse() extends DSyn //literal
+case class DNum(n: Int) extends DSyn //literal
+case class DPlus(l: DSyn, r: DSyn) extends DSyn
+case class DLeq(l: DSyn, r: DSyn) extends DSyn
+case class DId(c: String) extends DSyn
 
-sealed trait Command extends DExpr // not sure of this part yet
+sealed trait Command extends DSyn // not sure of this part yet
 case class DSeq(c1:Command, c2:Command) extends Command
-case class DAssign(id:DId, e:DExpr) extends Command
-case class DPrint(e: DExpr) extends Command
-case class DWhile(e:DExpr, c:Command) extends Command
+case class DAssign(id:DId, e:DSyn) extends Command
+case class DPrint(e: DSyn) extends Command
+case class DWhile(e:DSyn, c:Command) extends Command
 case class DDone() extends Command
 
 // final result, L*
@@ -45,7 +45,7 @@ object DExpr {
 object Step {
   //  def step(str: String): DExpr = parse(read(str))
 
-  def stepCommand(bexpr: BExpr): Command = bexpr match {
+  def stepCommand(bexpr: BSyn): Command = bexpr match {
     case BStr("done") => DDone()
     case BList(list) => list match {
       case l :: BStr(";") :: r :: Nil => DSeq(stepCommand(l), stepCommand(r))
@@ -61,7 +61,7 @@ object Step {
     case _ => throw new StepException("I don't know what to do with " + bexpr)
   }
 
-  def step(bexpr: BExpr): DExpr = bexpr match {
+  def step(bexpr: BSyn): DSyn = bexpr match {
     case BNum(n) => DNum(n)
     case BStr(s) => s match {
       case "true" => DTrue()
@@ -79,30 +79,36 @@ object Step {
 }
 
 object Derive {
-  case class Assign(id:String, value:DExpr)
+  case class Assign(id:String, value:Output)
   type Store = List[Assign]
 
   // not sure how to implement command yet
-  def derive(e: DExpr, st: Store): (Command, Output, Store) = e match {
+  def derive(e: DSyn, st: Store): (Command, Output, Store) = e match {
         case DSeq(c1, c2) => derive(c1, st) match {
           case (DDone(), out, st1) => derive(c2, st1)
+          case x => throw new DeriveException("DSeq failed on " + x)
         }
         case DAssign(DId(id), e) => {
-          val st1 = st ::: Assign(id, e) :: Nil
-          (DDone(), deriveExpr(e, st1)._1, st1)
+          val res = deriveExpr(e, st)
+          val st1 = st ::: Assign(id, res._1) :: Nil
+          (DDone(), res._1, st1)
         }
         case DPrint(e) => {
           (DDone(), deriveExpr(e, st)._1, st)
         }
         case DWhile(e, c) => deriveExpr(e, st) match {
-          case (BoolO(true), st1) => derive (c, st1)
+          case (BoolO(true), st1) =>
+            val res = derive(c, st1)
+            val next = derive(DWhile(e, c), res._3)
+            if (next._2 == NoneO()) (DDone(), res._2, res._3) else next
           case (BoolO(false), st1) => (DDone(), NoneO(), st1) // not sure about this one
+          case x => throw new DeriveException("invalid while condition, " + x + " is not a bool")
         }
         case DDone() => (DDone(), NoneO(), st) // not sure about this one either
         case x => throw new DeriveException(x + " command invalid")
   }
 
-  def deriveExpr(e: DExpr, st: Store): (Output, Store) = e match {
+  def deriveExpr(e: DSyn, st: Store): (Output, Store) = e match {
     case DTrue() => (BoolO(true), st)
     case DFalse() => (BoolO(false), st)
     case DNum(n) => (NumO(n), st)
@@ -120,10 +126,9 @@ object Derive {
 
   def lookup(x: String, st: Store): Output = st match {
     case Nil => throw new DeriveException("could not find identifier " + x + " in store")
-    case Assign(id, value) :: st1 => if (x == id) deriveExpr(value, st)._1 else lookup (x, st1)
+    case Assign(id, value) :: st1 => if (x == id) value else lookup (x, st1)
   }
 
-  def deriveExpr(e: DExpr): (Output, Store) = deriveExpr(e, Nil)
-  def derive(e: DExpr): (Command, Output, Store) = derive(e, Nil)
+  def deriveExpr(e: DSyn): (Output, Store) = deriveExpr(e, Nil)
+  def derive(e: DSyn): (Command, Output, Store) = derive(e, Nil)
 }
-
